256cd71d59c10b865cf66a764662fcc153d1ba3c
K.Jahnavi Priya <kummaraguntajunnu@gmail.com>
2025-11-27 11:45:35 +0530

    Add increment and decrement operations in C

---
+++ increement and decreement .c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int a = 10, b = 100;
+    float c = 12.5, d = 121.6;
+    printf("a = %d \n", ++a);
+    printf("b = %d \n", --b);
+    printf("c = %f \n", ++c);
+    printf("d = %f \n", --d);
+    return 0;
+}
